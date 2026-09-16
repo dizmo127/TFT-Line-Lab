476d6ac9e4b60d992a45e43e254daed3b91df53a
dizmo127 <dimeil.betsargon@gmail.com>
2026-09-15 20:14:01 -0700

    Cache opener highlight update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='tft-line-lab-v2';
-const FILES=['./','./index.html','./styles.css','./data.js','./wincons.js','./app.js','./manifest.webmanifest'];
+const CACHE='tft-line-lab-v3';
+const FILES=['./','./index.html','./styles.css','./data.js','./wincons.js','./app.js','./highlight.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
